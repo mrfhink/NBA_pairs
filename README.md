4dd07081d38ea0fb277eb8d6233b557eb4e0d561
mrfhink <mrfhink@gmail.com>
2022-02-01 16:26:35 -0500

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# NBA_pairs
-Mach Eight Sample Project
+#Task
+The task is to create an application that takes a single integer input. The application will download the raw data from the website above (https://mach-eight.uc.r.appspot.com) and print a list of all pairs of players whose height in inches adds up to the integer input to the application. If no matches are found, the application will print "No matches found"
+
+Sample output is as follows:
+
+	> app 139
+		
+	- Brevin Knight         Nate Robinson
+	- Nate Robinson         Mike Wilks
+
+
+The script asks for a number which is going to be searched by adding 2 heights values in the height list. The script will not iterate n*n times. We are assuming that we only need an unordered combination of heights which being added up will result in the number entered by the user. 
+
+![Script Iteration](/home/fhink/Pictures/Screenshot from 2022-02-01 16-23-10.png  "Script Iteration")
+
+Run 
+
+	python main.py
+	
+Code will ask for the number to search
+
+	Please enter the desired number...
+	Number:
+
+The code will show the pairs of players whose height in inches adds up to the integer input adn the number of matches found.
+
+	Number: 139
+	Pair found: [['Brevin Knight'], ['Nate Robinson']] heights [70, 69] add up 139
+	Pair found: [['Nate Robinson'], ['Mike Wilks']] heights [69, 70] add up 139
+	Matches found: 2
+
+
+If there are not pairs that satisfies the requested number the result will be:
+
+	Number: 180
+	Not matches found
---
+++ main.py
@@ -0,0 +1,46 @@
+import json
+from urllib.request import urlopen
+
+def find_pairs(data_nba: dict, target: int, partial_sum=[], pair=[], result=[]):
+    """
+    :param result: result list containing first name, last name and heights of pairs found
+    :param partial_sum: internal variable that sums a pair of heights to compare with the target
+    :param data_nba: dict read from json file containing nba players data
+    :param target: number defined by user to find pairs
+    :param pair: internal variable that holds first and last names of the pairs
+    :return: result
+    """
+    x = sum(partial_sum)
+    if sum(partial_sum) == target and len(pair) == 2:
+        print("Pair found: %s heights %s add up %s" % (pair, partial_sum, target))
+        return [pair[0], pair[1], partial_sum[0], partial_sum[1]] #Getting out of recursivity
+    if sum(partial_sum) != target and len(pair) == 2:
+        return [] #Getting out of recursivity
+
+    for i in range(len(data_nba)):
+
+        player_height = int(data_nba[i].get("h_in"))
+        remaining_data = data_nba[i + 1:]
+        player_name = [str(data_nba[i].get("first_name") + " " + data_nba[i].get("last_name"))]
+
+        temp = find_pairs(remaining_data, target, partial_sum + [player_height], pair + [player_name])
+
+        if len(temp) == 4:
+            result.append(temp)
+    return result
+
+if __name__ == "__main__":
+
+    print('Please enter the desired number...')
+    target_height_sum = int(input('Number: '))
+
+    url = "https://mach-eight.uc.r.appspot.com/"
+    response = urlopen(url)
+    data_nba = json.loads(response.read()).get('values')
+
+    result = find_pairs(data_nba, target_height_sum)
+
+    if not result:
+        print("Not matches found")
+    else:
+        print("Matches found: %s" % len(result))
